85eb01a1f682d0f3a0a42e36df47c63a6b2ac0db
Henry Alpert <halpert3@gmail.com>
2021-06-25 14:22:36 -0500

    setup SQL to run in Jupyter

--- code_wars_sql.py
+++ code_wars_sql.py
@@ -1,4 +1,41 @@
 """
+You have access to a table of monsters as follows:
+
+** monsters table schema **
+
+id
+name
+legs
+arms
+characteristics
+Your task is to make a new table where each column should contain the length of the string in the column to its right 
+(last column should contain length of string in the first column). Remember some column values are not currently strings.
+Column order and titles should remain unchanged:
+
+** output table schema **
+
+id
+name
+legs
+arms
+characteristics
+""" 
+SELECT 
+  LENGTH(name) AS id,
+  LENGTH(CAST(legs AS text)) AS name,
+  LENGTH(CAST(arms AS text)) AS legs,
+  LENGTH(characteristics) AS arms,
+  LENGTH(CAST(id AS text)) AS characteristics
+FROM monsters
+
+## A short cut for CAST seems to be ::
+# LENGTH(legs::text) AS name
+
+
+
+
+
+"""
 SQL Basics: Raise to the Power - 7th kyu
 
 Given the following table 'decimals':
